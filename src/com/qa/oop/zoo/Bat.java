18097813fd3987bd2cb680c3bce209369c57db87
jordan <jharrison1@qa.com>
2022-06-30 12:23:09 +0100

    updated bat

--- src/com/qa/oop/zoo/Bat.java
+++ src/com/qa/oop/zoo/Bat.java
@@ -2,13 +2,17 @@ package com.qa.oop.zoo;
 
 public class Bat extends Mammal {
 
+	private boolean rabid;
+
 	public Bat() {
 		super(2, true, false, true, false, "Blud");
 
 	}
 
-	public Bat(int noOfLegs, boolean canFly, boolean hasScales, boolean hasFur, boolean coldBlooded, String diet) {
+	public Bat(int noOfLegs, boolean canFly, boolean hasScales, boolean hasFur, boolean coldBlooded, String diet,
+			boolean rabid) {
 		super(noOfLegs, canFly, hasScales, hasFur, coldBlooded, diet);
+		this.rabid = rabid;
 	}
 
 	@Override
@@ -19,4 +23,13 @@ public class Bat extends Mammal {
 	public void fly() {
 		System.out.println("flap flap");
 	}
+
+	public boolean isRabid() {
+		return this.rabid;
+	}
+
+	public void setRabid(boolean rabid) {
+		this.rabid = rabid;
+	}
+
 }
